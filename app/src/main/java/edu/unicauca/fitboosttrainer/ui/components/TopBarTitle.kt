41109067c0e8acfca5c24ca9287e26f6a1dffdde
danie <danddiaz@unicauca.edu.co>
2024-10-21 10:32:54 -0500

    Top Bar solo titulo

---
+++ app/src/main/java/edu/unicauca/fitboosttrainer/ui/components/TopBarTitle.kt
@@ -0,0 +1,23 @@
+package edu.unicauca.fitboosttrainer.ui.components
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarTitle(title: String) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        navigationIcon = {},
+        actions = {}
+    )
+}
