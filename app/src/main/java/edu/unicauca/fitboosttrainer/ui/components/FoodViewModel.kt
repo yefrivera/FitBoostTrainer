58e934df9d6b36dcea79e1115b1903dfbdbe525a
yefrivera <yvera@unicauca.edu.co>
2024-10-22 02:54:11 -0500

    pantalla de calorias funcional, agrega y muestra datos

--- app/src/main/java/edu/unicauca/fitboosttrainer/ui/components/FoodViewModel.kt
+++ app/src/main/java/edu/unicauca/fitboosttrainer/ui/components/FoodViewModel.kt
@@ -1,18 +1,29 @@
 package edu.unicauca.fitboosttrainer.ui.components
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import kotlinx.coroutines.tasks.await
 
 class FoodViewModel : ViewModel() {
     var foodName = mutableStateOf("")
     var foodGrams = mutableStateOf("")
     var foodCalories = mutableStateOf("")
-    var mealType = mutableStateOf("Desayuno")
+    //var mealType = mutableStateOf("Desayuno")
 
-    // Instancia de Firebase Firestore
+    var addedFoods = mutableStateListOf<Map<String, Any>>()
+
     private val db = FirebaseFirestore.getInstance()
+    private var firestoreListener: ListenerRegistration? = null
+
+    var dailyGoalCalories = mutableStateOf(1000)
+    var totalCalories = mutableStateOf(0)
+
+    init {
+        fetchAddedFoods()
+    }
 
     fun setFoodName(newName: String) {
         foodName.value = newName
@@ -26,37 +37,90 @@ class FoodViewModel : ViewModel() {
         foodCalories.value = newCalories
     }
 
-    fun setMealType(newMealType: String) {
+    /*fun setMealType(newMealType: String) {
         mealType.value = newMealType
-    }
+    }*/
 
-    // Función para limpiar los campos
     fun clearFields() {
         foodName.value = ""
         foodGrams.value = ""
         foodCalories.value = ""
-        mealType.value = "Desayuno"
+        //mealType.value = "Desayuno"
     }
 
-    // Función para agregar los datos a Firebase Firestore
     suspend fun addFoodToFirebase(): Boolean {
-        // Crear el mapa de datos a enviar
         val foodData = hashMapOf(
             "name" to foodName.value,
             "grams" to foodGrams.value,
             "calories" to foodCalories.value,
-            "mealType" to mealType.value
+            //"mealType" to mealType.value
         )
 
         return try {
             db.collection("comidas")
-                .document("comidas")  // Refieres al documento llamado "comidas" dentro de la colección
-                .set(foodData)        // En lugar de .add() usas .set() para establecer los datos en ese documento
-                .await() // Espera que la tarea termine
+                .add(foodData)
+                .await()
             true
         } catch (e: Exception) {
-            // En caso de error, regresa falso
             false
         }
+    }
+
+    private fun fetchAddedFoods() {
+        firestoreListener = db.collection("comidas")
+            .addSnapshotListener { snapshots, e ->
+                if (e != null) return@addSnapshotListener
+                addedFoods.clear()
+                var total = 0
+
+                snapshots?.documents?.forEach { document ->
+                    document.data?.let { food ->
+                        addedFoods.add(food.plus("id" to document.id))
+                        total += (food["calories"].toString().toIntOrNull() ?: 0)
+                    }
+                }
+
+                totalCalories.value = total
+            }
+    }
+
+    fun updateFoodInFirebase(updatedFood: Map<String, Any>) {
+        val documentId = updatedFood["id"].toString()
+        db.collection("comidas")
+            .document(documentId)
+            .update(
+                "name", updatedFood["name"].toString(),
+                "grams", updatedFood["grams"].toString(),
+                "calories", updatedFood["calories"].toString(),
+                //"mealType", updatedFood["mealType"].toString()
+            )
+            .addOnSuccessListener {
+                val index = addedFoods.indexOfFirst { it["id"] == documentId }
+                if (index != -1) {
+                    addedFoods[index] = updatedFood
+                }
+            }
+    }
+
+    fun deleteFoodFromFirebase(food: Map<String, Any>) {
+        val documentId = food["id"].toString()
+        db.collection("comidas")
+            .document(documentId)
+            .delete()
+            .addOnSuccessListener {
+                addedFoods.remove(food)
+            }
+            .addOnFailureListener {
+
+            }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        firestoreListener?.remove()
+    }
+
+    fun updateDailyGoalCalories(newGoal: Int) {
+        dailyGoalCalories.value = newGoal
     }
 }
